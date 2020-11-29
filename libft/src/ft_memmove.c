28ecaffd0686054fd57ef3dce575b2994f69ed04
Mikhail Gorbachev <m.gorbachev@joinsprouttherapy.com>
2020-11-29 21:54:43 +0300

    Fix memmove

--- libft/src/ft_memmove.c
+++ libft/src/ft_memmove.c
@@ -12,106 +12,74 @@
 
 #include "../inc/libft.h"
 
-void	*ft_memmove_less_post(register char *dst, register
-		const char *src, size_t length, register size_t t)
+void ft_memmove_less(register char *dst, register const char *src, size_t length)
 {
-	t = length / WSIZE;
-	while (t)
-	{
-		*(int *)dst = *(int *)src;
-		src += WSIZE;
-		dst += WSIZE;
-		if (--t == 0)
-			break ;
-	}
-	t = length & WMASK;
-	while (t)
-	{
-		*--dst = *--src;
-		if (--t == 0)
-			break ;
-	}
-	return (dst);
+    register size_t t;
+
+    t = (int)src;
+    if ((t | (int)dst) & WMASK)
+    {
+        if ((t ^ (int)dst) & WMASK || length < WSIZE)
+            t = length;
+        else
+            t = WSIZE - (t & WMASK);
+        length -= t;
+        while (t-- != 0)
+            *dst++ = *src++;
+    }
+    t = length / WSIZE;
+    while (t-- != 0)
+    {
+        *(int *)dst = *(int *)src;
+        src += WSIZE;
+        dst += WSIZE;
+    }
+    t = length & WMASK;
+    while (t-- != 0)
+        *dst++ = *src++;
 }
 
-void	*ft_memmove_less(register char *dst, register const
-		char *src, size_t length, register size_t t)
+void ft_memmove_greater(register char *dst, register const char *src, size_t length)
 {
-	t = (int)src;
-	if ((t | (int)dst) & WMASK)
-	{
-		if ((t ^ (int)dst) & WMASK || length < WSIZE)
-			t = length;
-		else
-			t = WSIZE - (t & WMASK);
-		length -= t;
-		while (1)
-		{
-			if ((*dst++ = *src++) && --t == 0)
-				break ;
-		}
-	}
-	return (ft_memmove_less_post(dst, src, length, t));
+    register size_t t;
+
+    src += length;
+    dst += length;
+    t = (int)src;
+    if ((t | (int)dst) & WMASK)
+    {
+        if ((t ^ (int)dst) & WMASK || length <= WSIZE)
+            t = length;
+        else
+            t &= WMASK;
+        length -= t;
+        while (t-- != 0)
+            *--dst = *--src;
+    }
+    t = length / WSIZE;
+    while (t-- != 0)
+    {
+        src -= WSIZE;
+        dst -= WSIZE;
+        *(int *)dst = *(int *)src;
+    }
+    t = length & WMASK;
+    while (t-- != 0)
+        *--dst = *--src;
 }
 
-void	*ft_memmove_greater_post(register char *dst, const register
-		char *src, size_t length, register size_t t)
+void * ft_memmove(void *dst0, const void *src0, size_t length)
 {
-	t = length / WSIZE;
-	while (t)
-	{
-		src -= WSIZE;
-		dst -= WSIZE;
-		*(int *)dst = *(int *)src;
-		if (--t == 0)
-			break ;
-	}
-	t = length & WMASK;
-	while (t)
-	{
-		*--dst = *--src;
-		if (--t == 0)
-			break ;
-	}
-	return (dst);
-}
+    register char *dst = dst0;
+    register const char *src = src0;
 
-void	*ft_memmove_greater(register char *dst, const
-		register char *src, size_t length)
-{
-	register size_t t;
+    if (length == 0 || dst == src)    /* nothing to do */
+        return (dst0);
 
-	src += length;
-	dst += length;
-	t = (int)src;
-	if ((t | (int)dst) & WMASK)
-	{
-		if ((t ^ (int)dst) & WMASK || length <= WSIZE)
-			t = length;
-		else
-			t &= WMASK;
-		length -= t;
-		while (t)
-		{
-			*--dst = *--src;
-			if (--t == 0)
-				break ;
-		}
-	}
-	return (ft_memmove_greater_post(dst, src, length, t));
-}
+    if ((unsigned long)dst < (unsigned long)src)
+        ft_memmove_less(dst, src, length);
+    else
+        ft_memmove_greater(dst, src, length);
 
-void	*ft_memmove(void *dst, const void *src, size_t length)
-{
-	register char			*dst1;
-	register const char		*src1;
-	register size_t			t;
-
-	dst1 = dst;
-	src1 = src;
-	if (length == 0 || dst1 == src1)
-		return (dst);
-	if ((unsigned long)dst1 < (unsigned long)src1)
-		return (ft_memmove_less(dst1, src1, length, t));
-	return (ft_memmove_greater(dst1, src1, length));
+    return (dst0);
 }
